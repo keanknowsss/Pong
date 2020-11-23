a18249ebb25dc01f6d3a5f173955fe54b0833f2f
Carla Goyena <cmmgoyena@iskolarngbayan.pup.edu.ph>
2020-11-23 14:57:19 +0800

    added comments

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1280 --sets the windows width
+WINDOW_HEIGHT = 720 --sets the windows height
 
-function love.load()
+function love.load() --initializes the windows height and
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
+        fullscreen = false, -- not fullscreen
+        resizable = false, --not not resizable
+        vsync = true --vertical sync for monitor refresh rate
     })
 end 
 
-function love.draw()
+function love.draw() --initializes the contents of the function love.load()
     love.graphics.printf(
-        'Hello Pong! The Day-0 Update',
+        'Hello Pong! The Day-0 Update', --prints the message inside the quoutation mark
         0,
-        WINDOW_HEIGHT / 2-6,
+        WINDOW_HEIGHT / 2-6, 
         WINDOW_WIDTH,
         'center')
 end
