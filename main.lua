c27cae3b16466966dbca82d71401fa752a1bc0d3
Carla Goyena <cmmgoyena@iskolarngbayan.pup.edu.ph>
2020-11-23 16:22:14 +0800

    added push.lua and keypress function

--- main.lua
+++ main.lua
@@ -1,19 +1,30 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280 --sets the windows width
 WINDOW_HEIGHT = 720 --sets the windows height
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
 function love.load() --initializes the windows height and
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false, -- not fullscreen
         resizable = false, --not not resizable
         vsync = true --vertical sync for monitor refresh rate
     })
 end 
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw() --initializes the contents of the function love.load()
-    love.graphics.printf(
-        'Hello Pong! The Day-0 Update', --prints the message inside the quoutation mark
-        0,
-        WINDOW_HEIGHT / 2-6, 
-        WINDOW_WIDTH,
-        'center')
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2-6, VIRTUAL_WIDTH, 'center')
+
+    push:apply('end')
 end
